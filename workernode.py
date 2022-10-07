e62ba907b0b60e7a378077d039bfaba1c26306e6
EKKOING <nicholas.lorentzen@gmail.com>
2022-10-06 22:39:16 -0400

    more verbosity and graceful error handling

--- workernode.py
+++ workernode.py
@@ -35,78 +35,86 @@ while True:
             genome = collection.find_one({"started_eval": False})
             if genome is None:
                 continue
-            generation = genome["generation"]
-            individual_num = genome["individual_num"]
-            species = genome["species"]
-            tracks = genome["tracks"]
-            num_tracks = len(tracks)
-            bonuses = np.zeros(num_tracks).tolist()
-            completions = np.zeros(num_tracks).tolist()
-            times = np.full(num_tracks, -1.0).tolist()
-            last_xs = np.zeros(num_tracks).tolist()
-            last_ys = np.zeros(num_tracks).tolist()
-            avg_speeds = np.zeros(num_tracks).tolist()
-            avg_completions_per_frame = np.zeros(num_tracks).tolist()
-            runtimes = np.zeros(num_tracks).tolist()
-            collection.update_one(
-                {"_id": genome["_id"]},
-                {
-                    "$set": {
-                        "started_eval": True,
-                        "started_at": datetime.now(),
-                        "bonus": bonuses,
-                        "completion": completions,
-                        "time": times,
-                        "runtime": runtimes,
-                        "x": last_xs,
-                        "y": last_ys,
-                        "avg_speed": avg_speeds,
-                        "avg_completion_per_frame": avg_completions_per_frame,
-                    }
-                },
-            )
-            print(f'Beginning evaluation of genome {individual_num} in generation {generation} on {tracks}!')
-            for track_num, track in enumerate(tracks):
-                port_num = randint(49152, 65535)
-                print(f"Starting Server on {port_num}! Track: {track}")
-                server = subprocess.Popen(
-                    [
-                        "./xpilots",
-                        "-map", f"{track}.xp",
-                        "-noQuit",
-                        "-maxClientsPerIP", "500",
-                        "-password", "test",
-                        "-worldlives", "999",
-                        "-fps", f"{fps}",
-                        "-contactPort", f"{port_num}",
-                    ]
+            try:
+                generation = genome["generation"]
+                individual_num = genome["individual_num"]
+                species = genome["species"]
+                tracks = genome["tracks"]
+                num_tracks = len(tracks)
+                bonuses = np.zeros(num_tracks).tolist()
+                completions = np.zeros(num_tracks).tolist()
+                times = np.full(num_tracks, -1.0).tolist()
+                last_xs = np.zeros(num_tracks).tolist()
+                last_ys = np.zeros(num_tracks).tolist()
+                avg_speeds = np.zeros(num_tracks).tolist()
+                avg_completions_per_frame = np.zeros(num_tracks).tolist()
+                runtimes = np.zeros(num_tracks).tolist()
+                collection.update_one(
+                    {"_id": genome["_id"]},
+                    {
+                        "$set": {
+                            "started_eval": True,
+                            "started_at": datetime.now(),
+                            "bonus": bonuses,
+                            "completion": completions,
+                            "time": times,
+                            "runtime": runtimes,
+                            "x": last_xs,
+                            "y": last_ys,
+                            "avg_speed": avg_speeds,
+                            "avg_completion_per_frame": avg_completions_per_frame,
+                        }
+                    },
                 )
-                sleep(3)
-                print("Starting Bot!")
-                bot = subprocess.Popen(
-                    [
-                        "python3",
-                        "workerclient.py",
-                        "-port", f"{port_num}",
-                        "-track", f"{track_num}",
-                        "-dbid", f"{str(genome['_id'])}",
-                        "-eval_length", f"{eval_length}",
-                    ]
+                print(f'Beginning evaluation of genome {individual_num} in generation {generation} on {tracks}!')
+                for track_num, track in enumerate(tracks):
+                    port_num = randint(49152, 65535)
+                    print(f"Starting Server on {port_num}! Track: {track}")
+                    server = subprocess.Popen(
+                        [
+                            "./xpilots",
+                            "-map", f"{track}.xp",
+                            "-noQuit",
+                            "-maxClientsPerIP", "500",
+                            "-password", "test",
+                            "-worldlives", "999",
+                            "-fps", f"{fps}",
+                            "-contactPort", f"{port_num}",
+                        ]
+                    )
+                    sleep(3)
+                    print("Starting Bot!")
+                    bot = subprocess.Popen(
+                        [
+                            "python3",
+                            "workerclient.py",
+                            "-port", f"{port_num}",
+                            "-track", f"{track_num}",
+                            "-dbid", f"{str(genome['_id'])}",
+                            "-eval_length", f"{eval_length}",
+                        ]
+                    )
+                    sleep(1)
+                    print("Waiting for Bot to finish!")
+                    bot_return_code = bot.wait()
+                    sleep(1)
+                    print(f"Bot finished with return code {bot_return_code}!")
+                    server.kill()
+                    sleep(1)
+                    print("Server killed!")
+                    print(f"=== Finished Track {track_num + 1}/{len(tracks)} ===")
+                collection.update_one(
+                    {"_id": genome["_id"]}, {"$set": {"finished_eval": True}}
                 )
-                sleep(1)
-                print("Waiting for Bot to finish!")
-                bot_return_code = bot.wait()
-                sleep(1)
-                print(f"Bot finished with return code {bot_return_code}!")
-                server.kill()
-                sleep(1)
-                print("Server killed!")
-                print(f"=== Finished Track {track_num + 1}/{len(tracks)} ===")
-            collection.update_one(
-                {"_id": genome["_id"]}, {"$set": {"finished_eval": True}}
-            )
-            print("=== Finished Eval Successfully ===")
-            waiting = False
+                print("=== Finished Eval Successfully ===")
+                waiting = False
+            except Exception as e:
+                collection.update_one(
+                    {"_id": genome["_id"]}, {"$set": {"started_eval": False}}
+                )
+                print(f"Error In Eval: {e}")
+                waiting = False
+                print("==================")
         else:
             if not waiting:
                 print("Waiting For Work Assignment!")
@@ -115,7 +123,6 @@ while True:
             ## Try to desync workers
             sleep(randint(5, 15))
     except Exception as e:
-        raise e
         print(f"Error: {e}")
         sleep(randint(5, 15))
         print("==================")
