e3cbdf6f8eb933f04cab53f4356dc8cf46fd371e
EKKOING <nicholas.lorentzen@gmail.com>
2022-10-15 02:26:19 -0400

    worker spawner

--- workernode.py
+++ workernode.py
@@ -47,108 +47,103 @@ hostname += f"_{args.instance}"
 print("=== Beginning Work Cycle ===")
 waiting = False
 while True:
-    try:
-        if collection.count_documents({"started_eval": False}) != 0:
-            genome = collection.find_one({"started_eval": False}, sort=[("generation", pymongo.ASCENDING), ("individual_num", pymongo.ASCENDING)])
-            if genome is None:
-                continue
-            try:
-                generation = genome["generation"]
-                individual_num = genome["individual_num"]
-                species = genome["species"]
-                tracks = genome["tracks"]
-                num_tracks = len(tracks)
-                bonuses = np.zeros(num_tracks).tolist()
-                completions = np.zeros(num_tracks).tolist()
-                times = np.full(num_tracks, -1.0).tolist()
-                last_xs = np.zeros(num_tracks).tolist()
-                last_ys = np.zeros(num_tracks).tolist()
-                avg_speeds = np.zeros(num_tracks).tolist()
-                avg_completions_per_frame = np.zeros(num_tracks).tolist()
-                runtimes = np.zeros(num_tracks).tolist()
-                collection.update_one(
-                    {"_id": genome["_id"]},
-                    {
-                        "$set": {
-                            "started_eval": True,
-                            "started_at": datetime.now(),
-                            "bonus": bonuses,
-                            "completion": completions,
-                            "time": times,
-                            "runtime": runtimes,
-                            "x": last_xs,
-                            "y": last_ys,
-                            "avg_speed": avg_speeds,
-                            "avg_completion_per_frame": avg_completions_per_frame,
-                            "frame_rate": 0.0,
-                            "hostname": hostname,
-                            "finished_eval": False
-                        }
-                    },
+    if collection.count_documents({"started_eval": False}) != 0:
+        genome = collection.find_one({"started_eval": False}, sort=[("generation", pymongo.ASCENDING), ("individual_num", pymongo.ASCENDING)])
+        if genome is None:
+            continue
+        try:
+            generation = genome["generation"]
+            individual_num = genome["individual_num"]
+            species = genome["species"]
+            tracks = genome["tracks"]
+            num_tracks = len(tracks)
+            bonuses = np.zeros(num_tracks).tolist()
+            completions = np.zeros(num_tracks).tolist()
+            times = np.full(num_tracks, -1.0).tolist()
+            last_xs = np.zeros(num_tracks).tolist()
+            last_ys = np.zeros(num_tracks).tolist()
+            avg_speeds = np.zeros(num_tracks).tolist()
+            avg_completions_per_frame = np.zeros(num_tracks).tolist()
+            runtimes = np.zeros(num_tracks).tolist()
+            collection.update_one(
+                {"_id": genome["_id"]},
+                {
+                    "$set": {
+                        "started_eval": True,
+                        "started_at": datetime.now(),
+                        "bonus": bonuses,
+                        "completion": completions,
+                        "time": times,
+                        "runtime": runtimes,
+                        "x": last_xs,
+                        "y": last_ys,
+                        "avg_speed": avg_speeds,
+                        "avg_completion_per_frame": avg_completions_per_frame,
+                        "frame_rate": 0.0,
+                        "hostname": hostname,
+                        "finished_eval": False
+                    }
+                },
+            )
+            print(f'Beginning evaluation of genome {individual_num} in generation {generation} on {tracks}!')
+            for track_num, track in enumerate(tracks):
+                port_num = randint(49152, 65535)
+                print(f"Starting Server on {port_num}! Track: {track}")
+                server = subprocess.Popen(
+                    [
+                        "./xpilots",
+                        "-map", f"{track}.xp",
+                        "-noQuit",
+                        "-maxClientsPerIP", "500",
+                        "-password", "test",
+                        "-worldlives", "999",
+                        "-fps", f"{fps}",
+                        "-contactPort", f"{port_num}",
+                    ]
                 )
-                print(f'Beginning evaluation of genome {individual_num} in generation {generation} on {tracks}!')
-                for track_num, track in enumerate(tracks):
-                    port_num = randint(49152, 65535)
-                    print(f"Starting Server on {port_num}! Track: {track}")
-                    server = subprocess.Popen(
-                        [
-                            "./xpilots",
-                            "-map", f"{track}.xp",
-                            "-noQuit",
-                            "-maxClientsPerIP", "500",
-                            "-password", "test",
-                            "-worldlives", "999",
-                            "-fps", f"{fps}",
-                            "-contactPort", f"{port_num}",
-                        ]
-                    )
-                    sleep(3)
-                    print("Starting Bot!")
-                    bot = subprocess.Popen(
-                        [
-                            "python3",
-                            "workerclient.py",
-                            "-port", f"{port_num}",
-                            "-track", f"{track_num}",
-                            "-dbid", f"{str(genome['_id'])}",
-                            "-eval_length", f"{eval_length}",
-                        ]
-                    )
-                    sleep(1)
-                    print("Waiting for Bot to finish!")
-                    bot_return_code = bot.wait()
-                    sleep(1)
-                    print(f"Bot finished with return code {bot_return_code}!")
-                    if bot_return_code != 0:
-                        server.kill()
-                        server.wait()
-                        print("Server killed!")
-                        raise Exception("Bot Error!")
+                sleep(3)
+                print("Starting Bot!")
+                bot = subprocess.Popen(
+                    [
+                        "python3",
+                        "workerclient.py",
+                        "-port", f"{port_num}",
+                        "-track", f"{track_num}",
+                        "-dbid", f"{str(genome['_id'])}",
+                        "-eval_length", f"{eval_length}",
+                    ]
+                )
+                sleep(1)
+                print("Waiting for Bot to finish!")
+                bot_return_code = bot.wait()
+                sleep(1)
+                print(f"Bot finished with return code {bot_return_code}!")
+                if bot_return_code != 0:
                     server.kill()
-                    sleep(1)
+                    server.wait()
                     print("Server killed!")
-                    print(f"=== Finished Track {track_num + 1}/{len(tracks)} ===")
-                collection.update_one(
-                    {"_id": genome["_id"]}, {"$set": {"finished_eval": True}}
-                )
-                print("=== Finished Eval Successfully ===")
-                waiting = False
-            except Exception as e:
-                collection.update_one(
-                    {"_id": genome["_id"]}, {"$set": {"started_eval": False, "error": str(e)}}
-                )
-                print(f"Error In Eval: {e}")
-                waiting = False
-                print("==================")
-        else:
-            if not waiting:
-                print("Waiting For Work Assignment!")
-                print("==================")
-                waiting = True
-            ## Try to desync workers
-            sleep(uniform(3, 15))
-    except Exception as e:
-        print(f"Error: {e}")
+                    raise Exception("Bot Error!")
+                server.kill()
+                sleep(1)
+                print("Server killed!")
+                print(f"=== Finished Track {track_num + 1}/{len(tracks)} ===")
+            collection.update_one(
+                {"_id": genome["_id"]}, {"$set": {"finished_eval": True}}
+            )
+            print("=== Finished Eval Successfully ===")
+            waiting = False
+        except Exception as e:
+            collection.update_one(
+                {"_id": genome["_id"]}, {"$set": {"started_eval": False, "finished_eval": False, "error": str(e)}}
+            )
+            print(f"Error In Eval: {e}")
+            print("==================")
+            raise e
+    else:
+        if not waiting:
+            print("Waiting For Work Assignment!")
+            print("==================")
+            waiting = True
+        ## Try to desync workers
         sleep(uniform(3, 15))
-        print("==================")
     sleep(1)
---
+++ workernode_spawner.py
@@ -0,0 +1,24 @@
+import subprocess
+from time import sleep
+import argparse
+from typing import List
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-instances", help="num instances", required=True)
+parser.add_argument("-host", help="host", required=True)
+
+args = parser.parse_args()
+
+workers: List[subprocess.Popen] = []
+for i in range(int(args.instances)):
+    workers.append(subprocess.Popen(["python3", "workernode.py", "-instance", str(i)]))
+    sleep(1)
+
+while True:
+    for i, worker in enumerate(workers):
+        if worker.poll() is not None:
+            print(f"Worker {i} died!")
+            worker.kill()
+            worker.terminate()
+            workers[i] = subprocess.Popen(["python3", "workernode.py", "-instance", str(i)])
+    sleep(1)
