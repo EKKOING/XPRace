f24e0995a7771956e205060d531fc3b662474264
EKKOING <nicholas.lorentzen@gmail.com>
2022-12-12 17:09:06 -0500

    quick fix for genome query

--- run_saved_agent.py
+++ run_saved_agent.py
@@ -29,59 +29,63 @@ client = pymongo.MongoClient(db_string)
 db = client.NEAT
 collection = db.genomes
 
-trial = input("Trial Number: ")
-generation = input("Generation: ")
+trial = int(input("Trial Number: "))
+generation = int(input("Generation: "))
+individual_num = int(input("Individual Number: "))
 
-genome = collection.find_one({"trial": trial, "generation": generation}, sort=[("completion", pymongo.DESCENDING), ("time", pymongo.ASCENDING), ("bonus", pymongo.DESCENDING)])
+genome = collection.find_one({"trial": trial, "generation": generation, 'individual_num': individual_num})
+if genome is None:
+    print("Genome not found!")
+    exit()
 generation = genome["generation"]
 individual_num = genome["individual_num"]
 species = genome["species"]
 tracks = genome["tracks"]
 print(f'Running genome {individual_num} in generation {generation} on {tracks}!')
-for track_num, track in enumerate(tracks):
-    eval_length = 10
-    with open(f'{track}.json') as f:
-        track_info = json.load(f)
-        eval_length += track_info["target_time"]
-    port_num = randint(49152, 65535)
-    print(f"Starting Server on {port_num}! Track: {track}")
-    server = subprocess.Popen(
-        [
-            "./xpilots",
-            "-map", f"{track}.xp",
-            "-noQuit",
-            "-maxClientsPerIP", "500",
-            "-password", "test",
-            "-worldlives", "999",
-            "-fps", f"{fps}",
-            "-contactPort", f"{port_num}",
-        ]
-    )
-    sleep(3)
-    print("Starting Bot!")
-    bot = subprocess.Popen(
-        [
-            "python3",
-            "testclient.py",
-            "-port", f"{port_num}",
-            "-track", f"{track_num}",
-            "-dbid", f"{str(genome['_id'])}",
-            "-eval_length", f"{eval_length}",
-        ]
-    )
-    sleep(1)
-    print("Waiting for Bot to finish!")
-    bot_return_code = bot.wait()
-    sleep(1)
-    print(f"Bot finished with return code {bot_return_code}!")
-    if bot_return_code != 0:
+while True:
+    for track_num, track in enumerate(tracks):
+        eval_length = 10
+        with open(f'{track}.json') as f:
+            track_info = json.load(f)
+            eval_length += track_info["target_time"]
+        port_num = randint(49152, 65535)
+        print(f"Starting Server on {port_num}! Track: {track}")
+        server = subprocess.Popen(
+            [
+                "./xpilots",
+                "-map", f"{track}.xp",
+                "-noQuit",
+                "-maxClientsPerIP", "500",
+                "-password", "test",
+                "-worldlives", "999",
+                "-fps", f"{fps}",
+                "-contactPort", f"{port_num}",
+            ]
+        )
+        sleep(3)
+        print("Starting Bot!")
+        bot = subprocess.Popen(
+            [
+                "python3",
+                "testclient.py",
+                "-port", f"{port_num}",
+                "-track", f"{track_num}",
+                "-dbid", f"{str(genome['_id'])}",
+                "-eval_length", f"{eval_length}",
+            ]
+        )
+        sleep(1)
+        print("Waiting for Bot to finish!")
+        bot_return_code = bot.wait()
+        sleep(1)
+        print(f"Bot finished with return code {bot_return_code}!")
+        if bot_return_code != 0:
+            server.kill()
+            server.wait()
+            print("Server killed!")
+            raise Exception("Bot Error!")
         server.kill()
-        server.wait()
+        sleep(1)
         print("Server killed!")
-        raise Exception("Bot Error!")
-    server.kill()
-    sleep(1)
-    print("Server killed!")
-    print(f"=== Finished Track {track_num + 1}/{len(tracks)} ===")
-print("=== Finished Test Successfully ===")
-waiting = False
+        print(f"=== Finished Track {track_num + 1}/{len(tracks)} ===")
+    print("=== Finished Test Successfully ===")
